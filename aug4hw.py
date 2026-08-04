03f5cd47f6e54c6f7af132a57ff5b23e6aa87f5d
Sanjivanipande2004 <sanjivanipande2004@gmail.com>
2026-08-04 19:32:03 +0530

    Add files via upload

---
+++ aug4hw.py
@@ -0,0 +1,44 @@
+#Task1
+
+Name="Rahul"
+Age=20
+City="Pune"
+DOB="22-1-2000"
+College="TKA"
+Blood_Group="A+"
+
+print("------My ID Card------")
+print("Name:",Name)
+print("Age:",Age)
+print("City:",City)
+print("DOB:",DOB)
+print("College:",College)
+print("Blood Group:",Blood_Group)
+
+
+
+#Task2
+
+a=25
+b=25.5
+c="Twenty Five"
+d=True
+e=2+3j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
+
+#Task3
+
+v1=100
+v2=200
+v3=100
+
+print(id(v1))
+print(id(v2))
+print(id(v3))
